29eaee5c7bdf6fc4c92dc04e71453f358c28df76
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-01-23 16:54:22 +0100

    project setup done

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		}
+	}
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss">
+	@use './components/styles/partials/variables' as*;
+	@use './components/styles/partials/mixins' as*;
+
+	@use './components/styles/generals.scss' as*;
+</style>
