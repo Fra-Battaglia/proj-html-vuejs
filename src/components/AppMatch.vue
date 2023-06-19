4c992285ca14ff42115697a2a162d4d35500a013
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-06-19 19:53:40 +0200

    matches section done

---
+++ src/components/AppMatch.vue
@@ -0,0 +1,82 @@
+<script>
+	export default {
+		props: {
+			game: String,
+			date: String,
+			team_1: String,
+			team_1_img: String,
+			team_2: String,
+			team_2_img: String
+		},
+	}
+</script>
+
+<template>
+	<div class="match">
+		<div class="teams d-flex justify-content-between align-items-center">
+			<img :src="'/src/assets/img/teams/' + team_1_img" :alt="team_1" />
+			<img src="/src/assets/img/verus-icon.png" alt="versus" />
+			<img :src="'/src/assets/img/teams/' + team_2_img" :alt="team_2" />
+		</div>
+
+		<div class="match-info">
+			<h4>{{ game }}</h4>
+			<p>{{ date }}</p>
+		</div>
+
+		<div class="streaming-platforms d-flex justify-content-center gap-4">
+			<a href="#">
+				<img src="../assets/img/icon/youtube-icon.png" alt="youtube">
+				Youtube
+			</a>
+
+			<a href="#">
+				<img src="../assets/img/icon/twitch-icon.png" alt="twitch">
+				Twitch
+			</a>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	@use './styles/partials/variables' as*;
+	@use './styles/partials/mixins' as*;
+
+	.match {
+		background-color: #283449;
+		border: 1px solid #556f9c;
+		border-radius: 20px;
+		padding: 40px 0;
+		margin-bottom: 30px;
+		.teams {
+			padding: 0 50px;
+		}
+		.match-info {
+			background-color: #30405f;
+			padding: 30px 0;
+			margin: 40px 0;
+			text-align: center;
+
+			h4 {
+				font-family: $secondary-font;
+				font-size: 24px;
+				color: $light-color;
+				font-weight: 700;
+				margin: 0;
+			}
+
+			p {
+				font-size: 18px;
+				color: $primary-color;
+				margin: 15px 0 0 0;
+			}
+		}
+
+		.streaming-platforms {
+			a {
+				text-decoration: none;
+				color: $light-color;
+			}
+		}
+	}
+</style>
--- src/components/AppUpcomingMatches.vue
+++ src/components/AppUpcomingMatches.vue
@@ -1,8 +1,10 @@
 <script>
 	import AppButton from './AppButton.vue';
+	import AppMatch from './AppMatch.vue';
 	export default {
 		components: {
-			AppButton
+			AppButton,
+			AppMatch
 		},
 
 		data() {
@@ -11,26 +13,50 @@
 					{
 						game: 'Call Of Duty',
 						date: 'December 25, 2022 4:00 PM',
-						team_1: 'game-img-1.png',
-						team_2: 'game-img-2.png'
+						team_1: {
+								name: 'Cast Alligator',
+								img: 'game-img-1.png'
+							},
+						team_2: {
+							name: 'Gaming Bangke',
+							img: 'game-img-2.png'
+						},
 					},
 					{
 						game: 'Legendary Battles',
 						date: 'December 25, 2022 4:00 PM',
-						team_1: 'game-img-3.png',
-						team_2: 'game-img-4.png'
+						team_1: {
+							name: 'Gaming Esport',
+							img: 'game-img-3.png'
+						},
+						team_2: {
+							name: 'Ranger Gaming',
+							img: 'game-img-4.png'
+						},
 					},
 					{
 						game: 'Nay Corral Gundown',
 						date: 'December 25, 2022 4:00 PM',
-						team_1: 'game-img-5.png',
-						team_2: 'game-img-6.png'
+						team_1: {
+							name: 'Wolves',
+							img: 'game-img-5.png'
+						},
+						team_2: {
+							name: 'Esports Knights',
+							img: 'game-img-6.png'
+						},
 					},
 					{
-						game: 'League of Legends',
+						game: 'League Of Legends',
 						date: 'December 25, 2022 4:00 PM',
-						team_1: 'game-img-7.png',
-						team_2: 'game-img-8.png'
+						team_1: {
+							name: 'Holligans',
+							img: 'game-img-7.png'
+						},
+						team_2: {
+							name: 'Assassin',
+							img: 'game-img-8.png'
+						},
 					},
 				]
 			}
@@ -49,9 +75,8 @@
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-6"></div>
-				<div class="col-6">
-					
+				<div class="col-6" v-for="item in matches">
+					<AppMatch :game="item.game" :date="item.date" :team_1_img="item.team_1.img" :team_1="item.team_1.name" :team_2_img="item.team_2.img" :team_2="item.team_2.name"/>
 				</div>
 			</div>
 			<div class="row justify-content-center">
